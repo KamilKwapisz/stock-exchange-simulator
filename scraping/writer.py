db85bc736afc0db6b5146f3ff77948cbc1189597
KamilKwapisz <kamiltm1998@gmail.com>
2020-05-03 19:46:56 +0200

    removed prints

--- scraping/writer.py
+++ scraping/writer.py
@@ -1,6 +1,5 @@
 def write_data(data: list, output: str = 'stock_data.csv'):
     data = [",".join(row) for row in data]
     rows = [f"{row}\n" for row in data]
-    print(rows)
     with open(output, "w", encoding="utf-8") as csv_file:
         csv_file.writelines(rows)
