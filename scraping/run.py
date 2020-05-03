da27d81c90940b7b334d3f158ec3244ed160b446
KamilKwapisz <kamiltm1998@gmail.com>
2020-05-03 19:26:59 +0200

    running scraper refactored with Command Design Pattern

--- scraping/run.py
+++ scraping/run.py
@@ -1,8 +1,21 @@
 from scraper import StockScraper
 from writer import write_data
 
-url = "https://stooq.pl/t/?i=532"
 
-scraper = StockScraper()
-data = scraper.scrape(url)
-write_data(data)
+class ScrapeDataCommand:
+    """
+    Command to scrape new stock data from given URL
+    """
+    def __init__(self):
+        self.data = list()
+
+    def execute(self, url, output='stock_data.csv'):
+        scraper = StockScraper()
+        self.data = scraper.scrape(url)
+        write_data(self.data, output)
+
+
+if __name__ == "__main__":
+    url = "https://stooq.pl/t/?i=532"
+    cmd = ScrapeDataCommand()
+    cmd.execute(url)
