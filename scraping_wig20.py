a40b01fb1815ef05d4d2d0cd38c76a9b1cf195cb
KamilKwapisz <kamiltm1998@gmail.com>
2020-05-03 18:06:31 +0200

    added request header

--- scraping_wig20.py
+++ scraping_wig20.py
@@ -3,11 +3,15 @@ from bs4 import BeautifulSoup as Soup
 
 
 url = "https://stooq.pl/t/?i=532"
-r = requests.get(url)
+headers = {
+    'User-Agent': "Praca dyplomowa"
+}
+
+r = requests.get(url, headers=headers)
 soup = Soup(r.text, "html.parser")
 
 table = soup.find('tbody', {'align': 'right'})
 rows = table('tr')
 for row in rows[:2]:
-    for col in row('td'):
+    for col in row('td')[:3]:
         print(col.text.strip())
