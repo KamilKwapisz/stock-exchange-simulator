0d40435ad2d1bd37c1b1ae1a35ae9ac1ae674f2c
KamilKwapisz <kamiltm1998@gmail.com>
2020-05-24 11:11:35 +0200

    added transaction db model

--- stock_exchange/simulator/models.py
+++ stock_exchange/simulator/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext as _
 from djmoney.models.fields import MoneyField
 
 
@@ -20,3 +21,22 @@ class Stock(models.Model):
 
     def __str__(self):
         return f"{self.name}[{self.short_name}]"
+
+
+class Transaction(models.Model):
+    OPERATIONS = (
+       ('buy', _('Kupno')),
+       ('sell', _('Sprzedaż')),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+    operation = models.CharField(max_length=4, choices=OPERATIONS)
+    stocks_number = models.IntegerField()
+    stock_price = MoneyField(max_digits=14, decimal_places=2, default_currency='PLN')
+    # stoploss = MoneyField(max_digits=14, decimal_places=2, null=True, default_currency='PLN')
+
+    @property
+    def amount(self):
+        return self.stocks_number * self.stock_price
