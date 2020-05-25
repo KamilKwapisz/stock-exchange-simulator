54aca9786faab7d6cced8324400c228a250ff312
KamilKwapisz <kamiltm1998@gmail.com>
2020-05-25 16:53:30 +0200

    Changed the way of saving user actions

--- stock_exchange/simulator/models.py
+++ stock_exchange/simulator/models.py
@@ -7,7 +7,7 @@ from djmoney.models.fields import MoneyField
 class Account(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     balance = MoneyField(max_digits=14, decimal_places=2, default_currency='PLN')
-    stocks = models.ManyToManyField('Stock')
+    wallets = models.ManyToManyField('Wallet')
 
     def __str__(self):
         return f"{self.owner} account [{self.balance}]"
