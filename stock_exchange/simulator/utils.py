67dbd5864d421e673c72b1368097cc61de002273
KamilKwapisz <kamiltm1998@gmail.com>
2020-06-05 14:52:36 +0200

    function to calculate fee

--- stock_exchange/simulator/utils.py
+++ stock_exchange/simulator/utils.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from djmoney.money import Money
+
 from .models import Wallet
 
 
@@ -12,3 +15,14 @@ def save_wallets(account, stock, number):
         wallet.save()
         account.wallets.add(wallet)
         account.save()
+
+
+def calculate_fee(amount):
+    MINIMAL_FEE = float(getattr(settings, "MINIMAL_FEE", None))
+    PERCENTAGE_FEE = int(getattr(settings, "PERCENTAGE_FEE", None))
+
+    fee = amount * (PERCENTAGE_FEE / 100)
+    if fee < MINIMAL_FEE:
+        fee = MINIMAL_FEE
+    
+    return Money(fee, "PLN")
