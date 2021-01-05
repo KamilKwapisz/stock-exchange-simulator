fb77ad4a75af247e7860aba334ce52c6a9a20158
KamilKwapisz <kamiltm1998@gmail.com>
2021-01-05 22:06:23 +0100

    using MySQL backend

--- stock_exchange/stock_exchange/settings.py
+++ stock_exchange/stock_exchange/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'stock_exchange.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'USER': config('DB_USERNAME'),
+        'PASSWORD': config('DB_PASSWORD'),
     }
 }
 
