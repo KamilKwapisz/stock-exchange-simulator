b6e250e9b8be6ff929ef79caf960c4306d848870
KamilKwapisz <kamiltm1998@gmail.com>
2021-01-08 17:19:23 +0100

    using global URL

--- stock_exchange/scraping/run.py
+++ stock_exchange/scraping/run.py
@@ -4,6 +4,9 @@ from scraper import StockScraper, StockHistoryScraper
 from writer import write_data
 
 
+URL = "https://stooq.pl/t/?i=582"
+
+
 class ScrapeDataCommand:
     """
     Command to scrape new stock data from given URL
@@ -11,7 +14,7 @@ class ScrapeDataCommand:
     def __init__(self):
         self.data = list()
 
-    def execute(self, url, output='stock_data.csv'):
+    def execute(self, url=URL, output='stock_data.csv'):
         scraper = StockScraper()
         self.data = scraper.scrape(url)
         write_data(self.data, output)
@@ -24,18 +27,17 @@ class ScrapeHistoricalDataCommand:
     def __init__(self):
         self.data = list()
 
-    def execute(self, url, output='stock_data.csv'):
+    def execute(self, url=URL, output='stock_history_data.csv'):
         scraper = StockHistoryScraper()
         self.data = scraper.scrape(url)
         write_data(self.data, output, historical=True)
 
 
 if __name__ == "__main__":
-    url = "https://stooq.pl/t/?i=582"
     if argv[1] == 'history':
         output = "stock_history_data.csv"
         cmd = ScrapeHistoricalDataCommand()
     else:
         output = 'stock_data.csv'
         cmd = ScrapeDataCommand()
-    cmd.execute(url, output)
+    cmd.execute()
