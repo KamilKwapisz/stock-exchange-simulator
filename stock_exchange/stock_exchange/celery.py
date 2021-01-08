44c7fddc92371a3ecd8961c33496b69a20abb713
KamilKwapisz <kamiltm1998@gmail.com>
2021-01-08 17:50:58 +0100

    registered recurring scraping task

--- stock_exchange/stock_exchange/celery.py
+++ stock_exchange/stock_exchange/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock_exchange.settings')
 
@@ -14,3 +15,11 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+
+
+app.conf.beat_schedule = {
+    'regular-stock-data-scraping': {
+        'task': 'simulator.tasks.update_stock_data',
+        'schedule': crontab(minute=0, hour='*/3'),
+    },
+}
