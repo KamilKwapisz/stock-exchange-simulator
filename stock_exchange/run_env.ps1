f9134eb4c02f0d4d16c2e7175912c802b4c1ba5f
KamilKwapisz <kamiltm1998@gmail.com>
2021-01-09 13:51:54 +0100

    script for running env

---
+++ stock_exchange/run_env.ps1
@@ -0,0 +1,21 @@
+invoke-expression "cmd /c start powershell -NoExit -Command {
+    cd 'E:\!STUDIA\7 SEMESTR\Projekt Dyplomowy\stockapp\Scripts';
+    .\activate;
+    cd 'E:\!STUDIA\7 SEMESTR\Projekt Dyplomowy\stock_exchange';
+    python .\manage.py runserver 5005;
+}";
+
+invoke-expression "cmd /c start powershell -NoExit -Command {
+    cd 'E:\!STUDIA\7 SEMESTR\Projekt Dyplomowy\stockapp\Scripts';
+    .\activate;
+    cd 'E:\!STUDIA\7 SEMESTR\Projekt Dyplomowy\stock_exchange';
+    celery -A stock_exchange worker -l info;
+}";
+
+invoke-expression "cmd /c start powershell -NoExit -Command {
+    cd 'E:\!STUDIA\7 SEMESTR\Projekt Dyplomowy\stockapp\Scripts';
+    .\activate;
+    cd 'E:\!STUDIA\7 SEMESTR\Projekt Dyplomowy\stock_exchange';
+    celery -A stock_exchange beat;
+}";
+exit;
