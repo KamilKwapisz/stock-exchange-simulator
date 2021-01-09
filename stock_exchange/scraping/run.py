1f7a7274e735f4d90f9cf2fb99e8be8c22929eee
KamilKwapisz <kamiltm1998@gmail.com>
2021-01-09 22:13:20 +0100

    cmd for running news scraping

--- stock_exchange/scraping/run.py
+++ stock_exchange/scraping/run.py
@@ -1,10 +1,22 @@
 from sys import argv
 
-from .scraper import StockScraper, StockHistoryScraper
-from .writer import write_data
+from scraper import StockScraper, StockHistoryScraper, NewsScraper
+from writer import write_data
 
 
 URL = "https://stooq.pl/t/?i=582"
+STOCK_TICKERS = [
+    'ACP', 'ALE', 'ALR',
+    'ATT', 'CCC', 'CDR',
+    'CPS', 'DNP', 'EAT',
+    'ENA', 'EUR', 'ING',
+    'JSW', 'KER', 'KGH',
+    'KRU', 'KTY', 'LPP',
+    'LTS', 'MBK', 'MIL',
+    'OPL', 'PEO', 'PGE',
+    'PGN', 'PKN', 'PKO',
+    'PZU', 'SPL', 'TPE', 'WIG30'
+]
 
 
 class ScrapeDataCommand:
@@ -30,14 +42,34 @@ class ScrapeHistoricalDataCommand:
     def execute(self, url=URL, output='stock_history_data.csv'):
         scraper = StockHistoryScraper()
         self.data = scraper.scrape(url)
-        write_data(self.data, output, historical=True)
+        write_data(self.data, output, join_data=False)
+
+
+class NewsScraperDataCommand:
+    """
+    Command to scrape news for all companies
+    """
+    def __init__(self):
+        self.data = list()
+        self.base_url = "https://www.biznesradar.pl/wiadomosci"
+
+    def execute(self, stock_tickers: list, output='stock_news.csv') -> list:
+        scraper = NewsScraper()
+        for stock_ticker in stock_tickers:
+            URL = f"{self.base_url}/{stock_ticker}"
+            self.data += scraper.scrape(URL, stock_ticker)
+        write_data(self.data, output, separator='|')
 
 
 if __name__ == "__main__":
     if argv[1] == 'history':
         output = "stock_history_data.csv"
         cmd = ScrapeHistoricalDataCommand()
+        cmd.execute()
+    elif argv[1] == 'news':
+        cmd = NewsScraperDataCommand()
+        cmd.execute(STOCK_TICKERS)
     else:
         output = 'stock_data.csv'
         cmd = ScrapeDataCommand()
-    cmd.execute()
+        cmd.execute()
